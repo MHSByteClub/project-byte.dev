6f0877f374fbfc8121a87a0a507e20b5a9c8cee6
Michael Voell <sdcored@gmail.com>
2015-08-29 12:15:02 -0500

    Hero!
    Hero Image and Navbar started for main page.

--- index.php
+++ index.php
@@ -14,13 +14,24 @@
 
 <body>
 
-	<nav class="nav">
+	<!-- Regular Nav <nav class="nav">
 		<div class="nav-inner">
 			<a class="brand" href="#">Project Byte</a>
 			<a href="#">Home</a>
 			<a href="#">Browse</a>
 		</div>
-	</nav>
+	</nav> -->
+
+	<div class="hero-wrapper">
+		<div class="hero">
+			<div class="header_main">
+				<h2>Project Byte</h2>
+				<h3>A Collection of Bytes</h3>
+				<br /><br />
+				<a href="#" class="hero-button">View Projects</a>
+			</div>
+		</div>
+	</div>
 
 	<div class="container">
 		
@@ -60,6 +71,55 @@
 					<div class="card-content">
 						<div class="card-title">Project Title</div>
 						<p>Hi</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="section-header">Recent</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="card">
+							<img src="http://placehold.it/500x500?text=Text%20Is%20Cool" />
+							<div class="card-content">
+								<div class="card-title">Project Title</div>
+								<p>Hi</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<img src="http://placehold.it/500x500?text=Text%20Is%20Cool" />
+							<div class="card-content">
+								<div class="card-title">Project Title</div>
+								<p>Hi</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="section-header">Discussion</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="card">
+							<img src="http://placehold.it/500x500?text=Text%20Is%20Cool" />
+							<div class="card-content">
+								<div class="card-title">Project Title</div>
+								<p>Hi</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<img src="http://placehold.it/500x500?text=Text%20Is%20Cool" />
+							<div class="card-content">
+								<div class="card-title">Project Title</div>
+								<p>Hi</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
