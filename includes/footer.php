d6708e39548ed45f08a440b2945521d73e54fc1a
Michael voell <sdcored@gmail.com>
2015-08-31 16:28:26 +0000

    STuff

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
 		</div> -->
 		<div class="footer-copyright">
 			<div class="footer-inner">
-				&copy; HoloHex Corp. 2015 // <?php echo date("Y"); ?>. All rights reserved.
+				&copy; HoloHex Corp. 2015 // <?php echo date("Y"); ?>. All rights reserved. &middot; <a href="#">Terms of Service</a> &middot; <a href="#">Privacy Policy</a>
 			</div>
 		</div>
 	</footer>
