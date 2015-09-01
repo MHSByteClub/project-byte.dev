08533e8db938c86bbc55fb5ce5734adc4c98e322
Michael voell <sdcored@gmail.com>
2015-09-01 21:00:16 +0000

    Signin page design complete.

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
 		</div> -->
 		<div class="footer-copyright">
 			<div class="footer-inner">
-				&copy; HoloHex Corp. 2015 // <?php echo date("Y"); ?>. All rights reserved. &middot; <a href="#" style="color: #FFF;">Terms of Service</a> &middot; <a href="#" style="color: #FFF;">Privacy Policy</a>
+				&copy; <a href="http://holohex.net" target="_blank" style="color: #FFF;" />HoloHex Corp.</a> 2015 // <?php echo date("Y"); ?>. All rights reserved. &middot; <a href="#" style="color: #FFF;">Terms of Service</a> &middot; <a href="#" style="color: #FFF;">Privacy Policy</a>
 			</div>
 		</div>
 	</footer>
--- signin.php
+++ signin.php
@@ -2,6 +2,33 @@
 
     <div class="container">
         
+        <div class="row">
+            <div class="col-md-3">&nbsp;</div>
+            
+            <div class="col-md-6">
+                <div class="card">
+                <div class="card-content">
+                    <div class="card-title">Sign In</div>
+                    <p>
+                        <form action="" method="POST">
+                            <div class="row">
+                                <div class="col-sm-6 col-md-6">
+                                    <input type="text" name="username" id="username" placeholder="Username" />
+                                </div>
+                                <div class="col-sm-6 col-md-6">
+                                    <input type="password" name="password" id="password" placeholder="Password" />
+                                </div>
+                            </div>
+                            <input type="submit" name="subsign" id="subsign" value="Sign In" />
+                        </form>
+                    </p>
+                </div>
+            </div> 
+            
+            </div>
+            <div class="col-md-3">&nbsp;</div>
+        </div>
+        
     </div>
 
 <?php require_once("./includes/footer.php"); ?>
