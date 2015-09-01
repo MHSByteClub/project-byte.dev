3adceeacb1be44b62db9dc5a33b54bb2ffeb0286
Michael voell <sdcored@gmail.com>
2015-09-01 21:40:35 +0000

    Browse page design completed

--- browse.php
+++ browse.php
@@ -2,7 +2,111 @@
 
     <div class="container">
         
+        <div class="section-header">Categories</div>
         
+        <div class="row">
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Category</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Category</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Category</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Category</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Category</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Category</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="section-header">Most Recent</div>
+        
+        <div class="row">
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <img src="http://placehold.it/500x500" />
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Project</a></div>
+                        <p>Content</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <img src="http://placehold.it/500x500" />
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Project</a></div>
+                        <p>Content</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <img src="http://placehold.it/500x500" />
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Project</a></div>
+                        <p>Content</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <img src="http://placehold.it/500x500" />
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Project</a></div>
+                        <p>Content</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <img src="http://placehold.it/500x500" />
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Project</a></div>
+                        <p>Content</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="card hover">
+                    <img src="http://placehold.it/500x500" />
+                    <div class="card-content">
+                        <div class="card-title"><a href="#">Project</a></div>
+                        <p>Content</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </div>
 
